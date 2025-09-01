874428a0b6858b758ac90c0f1c6c005d23bbc1de
Emmanuel <www.proxynova95@gmail.com>
2025-09-01 22:02:38 +0300

    done

--- lib/regex.py
+++ lib/regex.py
@@ -1,13 +1,26 @@
 import re
 
-# NOTE: There are only a few tests included, so multiple solutions will work.
-# Feel free to encourage students to find oversights and add tests to this lab!
+# Name regex:
+# - Start with uppercase, then lowercase letters
+# - Allow optional parts with space, hyphen, or apostrophe followed by capitalized word
+# - Handles "John Cena", "Anya Taylor-Joy", "D'Angelo"
+name_regex = re.compile(
+    r"^[A-Z][a-z]*(?:[ -][A-Z][a-z]+|['’][A-Z][a-z]+)*$"
+)
 
-name = r""
-name_regex = re.compile(name)
+# Phone regex:
+# Matches:
+#   5555555555
+#   555-555-5555
+#   (555) 555-5555
+phone_regex = re.compile(
+    r"^(?:\(\d{3}\)\s?|\d{3}-?)\d{3}-?\d{4}$"
+)
 
-phone_number = r""
-phone_regex = re.compile(phone_number)
-
-email_address = r""
-email_regex = re.compile(email_address)
+# Email regex:
+# - Starts with a letter
+# - Allows letters, digits, and dots before @
+# - Requires @ + domain + TLD
+email_regex = re.compile(
+    r"^[A-Za-z][A-Za-z0-9.]*@[A-Za-z]+\.[A-Za-z]+$"
+)
